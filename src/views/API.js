60db7e699fb11e1cee76eafe164c409820a04b31
Addis <lumiaddis@gmail.com>
2024-03-19 22:31:19 -0300

    importe 2 funciones y reescribi la función de la api

--- src/views/API.js
+++ src/views/API.js
@@ -1,5 +1,9 @@
-export function API(props) {
+import { navigateTo } from "../router";
+import { footer } from "../components/Footer";
+
+export const apiKey = () => {
   const viewEl = document.createElement("div");
   viewEl.textContent = "Api Key";
+
   return viewEl;
 }
