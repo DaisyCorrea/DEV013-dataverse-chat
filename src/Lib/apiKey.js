13de0262907baa9737e1ad37d07bff423246d2c7
DaisyCorrea <daisy_jch@hotmail.com>
2024-03-25 18:53:48 -0500

    funciones APIKEY

--- src/Lib/apiKey.js
+++ src/Lib/apiKey.js
@@ -2,8 +2,13 @@
 
 export const getApiKey = () => {
     // Implementa el código para obtener la API KEY desde Local Storage
- };
+        const getStorage = localStorage.getItem("apiKey");
+        return getStorage;
+        /*console.log(getStorage); */
+}; 
  
- export const setApiKey = (key) => {
+ export const setApiKey = (Key) => {
    // Implementa el código para guardar la API KEY en Local Storage
+   const setStorage = localStorage.setItem("apiKey", Key);
+   return setStorage;
  };
--- src/Lib/openAIApi.js
+++ src/Lib/openAIApi.js
@@ -3,7 +3,15 @@
 // Importa la función para obtener la API KEY desde apiKey.js
 import { getApiKey } from './apiKey.js';
 
+const api_key = getApiKey();
 
-export const communicateWithOpenAI = (messages) => {
+export const communicateWithOpenAI = async (messages) => {
    //Aquí es donde debes implementar la petición con fetch o axios
+   try {
+    const respuesta = await fetch ('https://nnnnn')
+
+    
+   } catch (error){
+    console.log (error);
+   }
 };
