00767f66c8679141c8b777330ae7c4f24fbe9d7d
DaisyCorrea <daisy_jch@hotmail.com>
2024-03-30 22:21:45 -0500

    CSS en modal y vista error, borrado de evento click duplicado

--- src/views/Error.js
+++ src/views/Error.js
@@ -5,20 +5,33 @@ export const error = () => {
   const viewEl = document.createElement("div");
   viewEl.className = "errorBox";
   viewEl.appendChild(returnHome());
-   
+
+  const imgErrorDustTop = document.createElement ("img");
+  imgErrorDustTop.className = "errorDustTop";
+  imgErrorDustTop.src = "Images/duendes-del-polvo.png";
+  viewEl.appendChild(imgErrorDustTop);
+
   const imgError = document.createElement ("img");
-  imgError.className = "errorImagen";
+  imgError.className = "errorTotoro";
   imgError.src = "Images/totoro-paraguas.png";
   imgError.alt = "ERROR 404";
-  imgError.innerHTML = "ERROR 404";
-
   viewEl.appendChild(imgError);
   
-  const errorMessage = document.createElement("p");
-  errorMessage.className = "errorMessage";
-  errorMessage.innerHTML = "¡Alerta! Un pequeño Totoro travieso ha decidido esconder la página que estás buscando en su escondite secreto. ¿Quizás desees explorar otras sendas del bosque hasta encontrarla?";
-  viewEl.appendChild(errorMessage);
+  const errorMessageUno = document.createElement("p");
+  errorMessageUno.className = "errorMessageUno";
+  errorMessageUno.innerHTML = "ERROR 404";
+  viewEl.appendChild(errorMessageUno);
+  
+  const errorMessageTwo = document.createElement("p");
+  errorMessageTwo.className = "errorMessage";
+  errorMessageTwo.innerHTML = "¡Alerta! Un pequeño Totoro travieso ha decidido esconder la página que estás buscando en su escondite secreto. ¿Quizás desees explorar otras sendas del bosque hasta encontrarla?";
+  viewEl.appendChild(errorMessageTwo);
 
-  viewEl.appendChild(footer());
+  const imgErrorDustBottom = document.createElement ("img");
+  imgErrorDustBottom.className = "errorDustBottom";
+  imgErrorDustBottom.src = "Images/duendes-del-polvo.png";
+  viewEl.appendChild(imgErrorDustBottom);
+
+  //viewEl.appendChild(footer());
   return viewEl;
 };
