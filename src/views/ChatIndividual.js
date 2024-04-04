6ebd92fae03cc29b6b23275117fe9f6cb4da854a
DaisyCorrea <daisy_jch@hotmail.com>
2024-04-04 09:56:50 -0500

    cambios ChatIndividual

--- src/views/ChatIndividual.js
+++ src/views/ChatIndividual.js
@@ -1,7 +1,6 @@
 import { returnHome } from "../components/ButtonHome.js";
 import { footer } from "../components/Footer.js";
 import data from '../data/dataset.js';
-//import { navigateTo } from "../router.js";
 
 export const chatIndividual = (film) => {
   console.log(film, "prueba")
@@ -14,16 +13,13 @@ export const chatIndividual = (film) => {
   structure.setAttribute("class", "fondo");
   structure.innerHTML = `
     <br><h4> Soy ${findFil.name} </h4><br>
-    <img src=${findFil.imageUrl} alt="Aquí va la imagen ">
+    <img src=${findFil.imageUrl} alt=${findFil.name}>
         <div class="chat-box">
             <input type="text" class="inputChat" placeholder="Escribe...">
             <button class="sendMes"><i class="fas fa-location-arrow"></i></button>
         </div>
     `
-    
     viewIndividual.append(returnHome(), structure, footer());
-      
-  
     return viewIndividual;
 };
 
