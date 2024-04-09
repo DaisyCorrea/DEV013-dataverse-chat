3054ad417cba9cbded535704f6d65ab07234af1d
DaisyCorrea <daisy_jch@hotmail.com>
2024-04-09 17:50:10 -0500

    otros cambios y Readme

--- src/views/ChatGrupal.js
+++
@@ -1,9 +0,0 @@
-import { footer } from "../components/Footer.js";
-
-export const chatGrupal = () => {
-  const viewGrupal = document.createElement("div");
-  viewGrupal.setAttribute("class", "chat-gupal");
-  footer();
-  
-  return viewGrupal;
-}
--- test/openIA.spec.js
+++ test/openIA.spec.js
@@ -1,12 +1,22 @@
 import { communicateWithOpenAI } from "../src/Lib/openAIApi.js"
 
+/* eslint-diseable */
+
 global.fetch = jest.fn(() => {
     Promise.resolve({
         status: 200,
         json: () => Promise.resolve({}),
-    });
+    })
 });
 
-describe("funcion de OpenIA", => {
-    it('llamar a la API de OpenIA y o')
+describe("funcion de OpenAi",() => {
+    it('Debería responder la función communicateWithOpenAi', () =>{
+        const functionOpenAi = communicateWithOpenAI();
+        expect(functionOpenAi).toBe(Promise)  
+    })
 })
+
+
+
+
+
